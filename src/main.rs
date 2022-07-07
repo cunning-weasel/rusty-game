aefaecdb3a728529a690662d2a3052117d72add4
weasel <paulbongers22@gmail.com>
2022-07-07 22:34:47 +0200

    getting game state ready

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,29 @@
 use rusty_engine::prelude::*;
 
+// game state struct
+struct GameState {
+    high_score: u32,
+    current_score: u32,
+    enemy_labels: Vec<String>,
+    spawn_timer: Timer,
+}
+
+// default game state
+impl Default for GameState {
+    fn default() -> Self {
+        Self {
+            high_score: 0,
+            current_score: 0,
+            enemy_labels: Vec::new(),
+            spawn_timer: Timer::from_seconds(1.0, false),
+        }
+    }
+}
+
 fn main() {
     // mut as we need to keep track of game-state
     let mut game = Game::new();
 
     // setup game
-
-    game.run(());
+    game.run(GameState::default());
 }
-
