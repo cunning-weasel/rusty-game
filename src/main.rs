85826e1e906e47fbe7f5077266193eab246b718e
weasel <paulbongers22@gmail.com>
2022-07-07 23:05:02 +0200

    adding sprites, next up collisions

--- src/main.rs
+++ src/main.rs
@@ -25,5 +25,21 @@ fn main() {
     let mut game = Game::new();
 
     // setup game
+    let player = game.add_sprite("player", SpritePreset::RacingCarBlue);
+    player.translation = Vec2::new(200.0, -300.0);
+    player.rotation = RIGHT;
+    player.scale = 1.0;
+    player.layer = 999.0;
+
+    game.add_logic(game_logic);
     game.run(GameState::default());
+}
+
+fn game_logic(engine: &mut Engine, game_state: &mut GameState) {
+    // game logic
+
+
+
+    // game_state.current_score += 1;
+    // print!("current score: {}", game_state.current_score);
 }
