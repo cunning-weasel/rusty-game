11b31306598fe71468d3a60b4b40099e672130df
abc <paulbongers22@gmail.com>
2022-07-09 18:49:43 +0200

    adding obstacles

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use rand::prelude::*;
 use rusty_engine::prelude::*;
 
 // game state struct
@@ -16,13 +17,16 @@ impl Default for GameState {
     }
 }
 
+// glaobal road speed - check out game logic
+const ROAD_SPEED: f32 = 400.0;
+
 fn main() {
     // mut as we need to keep track of game-state
     let mut game = Game::new();
 
     // audio
     game.audio_manager
-        .play_music(MusicPreset::Classy8Bit, 0.1);
+        .play_music(MusicPreset::WhimsicalPopsicle, 0.1);
 
     // setup game
     let player1 = game.add_sprite("player1", SpritePreset::RacingCarBlue);
@@ -30,6 +34,29 @@ fn main() {
     player1.rotation = RIGHT;
     player1.collision = true;
     player1.layer = 10.0;
+
+    // road line moves left to create illusion of
+    // of car moving down the road
+    for i in 0..10 {
+        let roadline = game.add_sprite(format!("roadline{}", i), SpritePreset::RacingBarrierWhite);
+        roadline.scale = 0.1;
+        roadline.translation.x = -600.0 + 150.0 * i as f32;
+    }
+
+    // obstacles - more == harder
+    let obstacle_presets = vec![
+        SpritePreset::RacingBarrelBlue,
+        SpritePreset::RacingBarrelRed,
+        SpritePreset::RacingConeStraight,
+    ];
+
+    for (i, preset) in obstacle_presets.into_iter().enumerate() {
+        let obstacle = game.add_sprite(format!("obstacle{}", i), preset);
+        obstacle.layer = 5.0;
+        obstacle.collision = true;
+        obstacle.translation.x = thread_rng().gen_range(800.0..1600.0);
+        obstacle.translation.y = thread_rng().gen_range(-300.0..300.0);
+    }
 
     game.add_logic(game_logic);
     game.run(GameState::default());
@@ -45,6 +72,23 @@ fn game_logic(engine: &mut Engine, game_state: &mut GameState) {
         direction += 1.0;
     } else if engine.keyboard_state.pressed(KeyCode::Down) {
         direction -= 1.0;
+    }
+
+    // move road objects
+    for sprite in engine.sprites.values_mut() {
+        if sprite.label.starts_with("roadline") {
+            sprite.translation.x -= ROAD_SPEED * engine.delta_f32;
+        } else if sprite.translation.x < -675.0 {
+            sprite.translation.x += 1500.0;
+        }
+        // obstacles appearing on the road as they move
+        if sprite.label.starts_with("obstacle") {
+            sprite.translation.x -= ROAD_SPEED * engine.delta_f32;
+            if sprite.translation.x < -800.0 {
+                sprite.translation.x = thread_rng().gen_range(800.0..1600.0);
+                sprite.translation.y = thread_rng().gen_range(-300.0..300.0);
+            }
+        }
     }
 
     let player1 = engine.sprites.get_mut("player1").unwrap();
